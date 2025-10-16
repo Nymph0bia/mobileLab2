4b0d5957b575b8985df0c1047682d5d30e996023
Nymph0bia <swordmagicm@gmail.com>
2025-10-17 00:14:52 +0300

    Making Tests

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
 package com.example.diceroller
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.text.NumberFormat
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
     }
 }
